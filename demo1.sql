8cdea214ba8a569d04ac82c7367d6bb6a8ca355e
lucdzz <lucnthe173098@fpt.edu.vn>
2024-05-14 12:53:16 +0700

    Delete demo1.sql

--- demo1.sql
+++
@@ -1,9 +0,0 @@
-create table Admin
-(
-Username varchar(20) primary key,
-Password varchar(20),
-role int,
-)
-go
-insert into Admin values('admin','123',1)
-insert into Admin values('manager','123',2)
